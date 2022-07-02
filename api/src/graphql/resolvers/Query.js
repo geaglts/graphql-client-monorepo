801df7e500b99faa3ca744f09bf33dca1a1a2f13
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2022-07-02 09:08:00 -0500

    start script

--- api/src/graphql/resolvers/Query.js
+++ api/src/graphql/resolvers/Query.js
@@ -3,7 +3,6 @@ import prisma from '../../services/prisma';
 const Query = {
   clients: async () => {
     const clients = await prisma.client.findMany();
-    console.log(clients);
     return clients;
   },
 };
